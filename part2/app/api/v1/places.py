9ecd1e3ca9699f9706cb96c93ac2b1f4bf168f38
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-19 16:33:32 +0200

    last update with review

--- part2/app/api/v1/places.py
+++ part2/app/api/v1/places.py
@@ -39,7 +39,8 @@ place_model = api.model('Place', {
     'latitude': fields.Float(required=True, description='Latitude of the place'),
     'longitude': fields.Float(required=True, description='Longitude of the place'),
     'owner_id': fields.String(required=True, description='ID of the owner'),
-    'amenities': fields.List(fields.String, required=True, description="List of amenities ID's")
+    'amenities': fields.List(fields.Nested(amenity_model), description='List of amenities'),
+    'reviews': fields.List(fields.Nested(review_model), description='List of reviews')
 })
 
 @api.route('/')
