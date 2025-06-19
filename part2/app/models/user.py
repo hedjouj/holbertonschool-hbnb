f71c7968b243eb9e63c1d36cf5b08b1c36ebe932
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-19 10:44:57 +0200

    create user

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,17 +1,19 @@
-# app/models/user.py
-
 from app.models.base_model import BaseModel
 
-class User(BaseModel):
-    emails_seen = set()  # pour valider l'unicité des emails
 
-    def __init__(self, first_name: str, last_name: str, email: str, is_admin=False):
+class User(BaseModel):
+    emails_seen = set()  # pour valider les mails
+
+    def __init__(self, first_name: str, last_name: str, email: str,
+                 is_admin=False):
         super().__init__()
 
         if not first_name or len(first_name) > 50:
-            raise ValueError("First name is required and must be ≤ 50 characters.")
+            raise ValueError(
+                "First name is required and must be ≤ 50 characters.")
         if not last_name or len(last_name) > 50:
-            raise ValueError("Last name is required and must be ≤ 50 characters.")
+            raise ValueError(
+                "Last name is required and must be ≤ 50 characters.")
         if not email or len(email) > 100:
             raise ValueError("Email is required and must be ≤ 100 characters.")
         if email in User.emails_seen:
@@ -26,3 +28,9 @@ class User(BaseModel):
         self.places = []
 
         User.emails_seen.add(email)
+
+    def __str__(self):
+        """
+        Used to return object as we want
+        """
+        return "{} {}".format(self.first_name, self.last_name)
