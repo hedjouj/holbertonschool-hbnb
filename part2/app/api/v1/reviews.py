fd420bdfe3534b05668ac0449d679a88f76b4b0a
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-20 10:35:55 +0200

    try to fix bug user not found

--- part2/app/api/v1/reviews.py
+++ part2/app/api/v1/reviews.py
@@ -8,10 +8,10 @@ api = Namespace('reviews', description='Review operations')
 review_model = api.model(
     'Review', {
         'text': fields.String(required=True, description='Text of the review'),
+        'user_id': fields.String(required=True, description='ID of the user'),
+        'place_id': fields.String(required=True, description='ID of the place'),
         'rating': fields.Integer(required=True,
                                  description='Rating of the place (1-5)'),
-        'user_id': fields.String(required=True, description='ID of the user'),
-        'place_id': fields.String(required=True, description='ID of the place')
     }
 )
 
