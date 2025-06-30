75d1a8c229943ced9db5091176befa4a1c35ed30
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-30 14:50:43 +0200

    fix: use config_name to charge the right config from config.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_restx import Api
+from config import config
+
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as review_ns
 
 
-def create_app():
+def create_app(config_name='default'):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    
     api = Api(app, version='1.0', title='HBnB API', doc='/api/v1/',
               description='HBnB Application API')
     
