c5cac65b6d454a03a5eabcbc410a55f7e2f0a884
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-30 16:11:01 +0200

    fix: modify the argument in the create app

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from config import config
+from flask_jwt_extended import JWTManager
 
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -10,10 +11,11 @@ from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 
+jwt = JWTManager()
 
-def create_app(config_class="config.DevelopmentConfig"):
+def create_app(config_name='default'):
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    app.config.from_object(config[config_name])
     
     api = Api(app, version='1.0', title='HBnB API', doc='/api/v1/',
               description='HBnB Application API')
