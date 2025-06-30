c3f363c4d99d61942890744f804cd94244200c6f
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-30 16:22:19 +0200

    fix: add auth namespace to the API in create_app function

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -7,6 +7,8 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as review_ns
+from app.api.v1.auth import api as auth_ns
+
 
 jwt = JWTManager()
 
@@ -16,9 +18,11 @@ def create_app(config_class=config.DevelopmentConfig):
     
     api = Api(app, version='1.0', title='HBnB API', doc='/api/v1/',
               description='HBnB Application API')
-    
+    jwt.init_app(app)
+
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(review_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     return app
