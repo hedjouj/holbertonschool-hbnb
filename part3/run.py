28c2528c70499888ba9ec44c5db3a33b6d0f3df6
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-01 09:57:38 +0200

    fix: correct configuration name in create_app call

--- part3/run.py
+++ part3/run.py
@@ -3,10 +3,14 @@
 from app import create_app
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 app = create_app('development')
 =======
 app = create_app('DevelopmentConfig')
 >>>>>>> 52f027e (fix: update create_app function to use config_name and correct config reference)
+=======
+app = create_app('development')
+>>>>>>> 19486a6 (fix: correct configuration name in create_app call)
 
 if __name__ == '__main__':
     app.run(debug=True)
