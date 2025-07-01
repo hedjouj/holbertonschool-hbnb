26400203f5c8aab7378992793f82ea5cd099beb8
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-01 15:54:25 +0200

    fix: refactor bcrypt initialization and remove unused import

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_restx import Api
 from config import config
 from flask_jwt_extended import JWTManager
+
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as review_ns
 from app.api.v1.auth import api as auth_ns
 from flask_bcrypt import Bcrypt
-from app.extensions import bcrypt
+
+bcrypt = Bcrypt()
 
 jwt = JWTManager()
 
@@ -16,8 +18,8 @@ def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     jwt.init_app(app)
-    bcrypt.init_app(app)
 
+    
     api = Api(app, version='1.0', title='HBnB API', doc='/api/v1/',
               description='HBnB Application API')
 
@@ -26,5 +28,6 @@ def create_app(config_name='default'):
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(review_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
+    bcrypt.init_app(app)
     
     return app
