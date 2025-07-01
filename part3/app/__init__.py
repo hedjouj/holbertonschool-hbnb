d120c305faec365b4f612877b4dccc442f35c5b9
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-01 09:51:41 +0200

    fix: move jwt.init_app call to the correct position in create_app function

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -16,10 +16,11 @@ jwt = JWTManager()
 def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    jwt.init_app(app)
+
     
     api = Api(app, version='1.0', title='HBnB API', doc='/api/v1/',
               description='HBnB Application API')
-    jwt.init_app(app)
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
