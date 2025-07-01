af248e2b5a944827596033108f1823bfa77bb7e2
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-01 09:51:34 +0200

    fix: add JWT_SECRET_KEY to the Config class

--- part3/config.py
+++ part3/config.py
@@ -4,6 +4,9 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> bd0056d (fix: add JWT_SECRET_KEY to the Config class)
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt_secret')
     
 class DevelopmentConfig(Config):
