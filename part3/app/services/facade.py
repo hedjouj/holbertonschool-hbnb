57c287b51a8b2e868ba3145abd395e3bdb4383f7
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-03 10:21:25 +0200

    feat: changed the destination of the data to SQL

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -1,16 +1,15 @@
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import InMemoryRepository, SQLAlchemyRepository
 from app.models.user import User
 from app.models.place import Place
 from app.models.amenity import Amenity
 from app.models.review import Review
 
-
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
+        self.place_repo = SQLAlchemyRepository(Place)
+        self.review_repo = SQLAlchemyRepository(Review)
+        self.amenity_repo = SQLAlchemyRepository(Amenity)
 
     def create_place(self, place_data):
         """Create a new place."""
