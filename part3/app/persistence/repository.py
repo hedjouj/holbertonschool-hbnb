ed1cd0e7beb16a7bc0dcea55be86ca6d3f2f2a19
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-03 11:21:04 +0200

    feat: add UserRepository to handle user-specific queries, including retrieval by email

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from app import db  # Assuming you have set up SQLAlchemy in your Flask app
 from app.models import User, Place, Review, Amenity  # Import your models
+from app.persistence.repository import SQLAlchemyRepository
 
 
 class Repository(ABC):
@@ -92,3 +93,10 @@ class SQLAlchemyRepository(Repository):
 
     def get_by_attribute(self, attr_name, attr_value):
         return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
+    
+class UserRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(User)
+
+    def get_user_by_email(self, email):
+        return self.model.query.filter_by(email=email).first()
