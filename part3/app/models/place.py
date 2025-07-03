0e5b05d09b98e2e8bf4df2ccc22e508e6c6f8963
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-03 20:11:34 +0200

    fix: rework the code to make it more clean and added the table place

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -2,17 +2,17 @@ from app.models.base_model import BaseModel
 from app import db
 
 class Place(BaseModel):
+    __tablename__ = 'place'
 
-    __tablename__ = 'Place'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.Integer, nullable=False)
 
-    id = db.Column(db.Integer(50), primary_key=True )
-    title = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(120), nullable=False)
-    price = db.Column(db.Float(128), nullable=False)
-    latitude = db.Column(db.Float, default=False)
-    longitude = db.Column(db.Float, default=False)
-
-    def __init__(self, title: str, price: float, latitude: float, longitude: float, owner_id: str, amenities=[], reviews=[], description=""):
+    def __init__(self, title: str, price: float, latitude: float, longitude: float, owner_id: int, amenities=None, reviews=None, description=""):
         super().__init__()
 
         if not title or len(title) > 100:
@@ -23,8 +23,6 @@ class Place(BaseModel):
             raise ValueError("Latitude must be between -90 and 90.")
         if not (-180.0 <= longitude <= 180.0):
             raise ValueError("Longitude must be between -180 and 180.")
-        #if not isinstance(owner, User):
-        #    raise TypeError("Owner must be a valid User instance.")
 
         self.title = title
         self.description = description
@@ -32,24 +30,8 @@ class Place(BaseModel):
         self.latitude = latitude
         self.longitude = longitude
         self.owner_id = owner_id
-        self.reviews = reviews
-        self.amenities = amenities
-
-        #owner.places.append(self)
-
-    def to_dict(self):
-        """Convert the Place object to a dictionary."""
-        return {
-            "id": self.id,
-            "title": self.title,
-            "description": self.description,
-            "price": self.price,
-            "latitude": self.latitude,
-            "longitude": self.longitude,
-            "owner_id": self.owner_id,
-            "reviews": [review.to_dict() for review in self.reviews],
-            "amenities": self.amenities
-        }
+        self.reviews = reviews if reviews is not None else []
+        self.amenities = amenities if amenities is not None else []
 
     def to_dict(self):
         """Convert the Place object to a dictionary."""
