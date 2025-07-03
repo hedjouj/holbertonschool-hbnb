1161d7bf979ac85e340d165dac70fa5a343a5e8f
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-03 20:14:41 +0200

    fix: added amenity table

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -6,31 +6,20 @@ from app import db
 
 
 class Amenity(BaseModel):
+    __tablename__ = 'amenity'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
 
-    __tablename__='Amenity'
-    id = db.Column(db.Integer(50), primary_key=True )
-    title = db.Column(db.String(50), nullable=False)
-    
     '''Represents an amenity with attributes and restrictions'''
     def __init__(self, name):
         '''Initialize a new Amenity instance with restrictions'''
         super().__init__()
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str):
+        if not isinstance(name, str):
             raise TypeError("Amenity name must be a string of characters.")
-        if not value or len(value) > 50:
-            raise ValueError(
-                "Amenity name must be a string of 1 to 50 characters."
-            )
-        self._name = value
+        if not name or len(name) > 50:
+            raise ValueError("Amenity name must be a string of 1 to 50 characters.")
+        self.name = name
 
     def to_dict(self):
         """Convert the Amenity object to a dictionary."""
