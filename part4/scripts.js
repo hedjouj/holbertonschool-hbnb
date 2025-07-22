2dcc766d286f7734b285b3a64afc326fdf3964e0
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-22 15:55:58 +0200

    feat: added the function to get the data places

---
+++ part4/scripts.js
@@ -0,0 +1,36 @@
+/* 
+  This is a SAMPLE FILE to get you started.
+  Please, follow the project instructions to complete the tasks.
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+fetchPlaces();  
+});
+
+function fetchPlaces() {
+    fetch('http://localhost:5000/api/places')
+        .then(response => response.json())
+        .then(data => {
+            displayPlaces(data);
+        })
+        .catch(error => {
+            console.error('Error fetching places:', error);
+        });
+}
+  function displayPlaces(places) {
+    const placesList = document.getElementById('places-list');
+    placesList.innerHTML = '';
+
+    places.forEach(place => {
+        const card = document.createElement('div');
+        card.className = 'place-card';
+
+        card.innerHTML = `
+            <h3>${place.name}</h3>
+            <p>Price per night: $${place.price}</p>
+            <button class="details-button" data-id="${place.id}">View Details</button>
+        `;
+
+        placesList.appendChild(card);
+    });
+}
