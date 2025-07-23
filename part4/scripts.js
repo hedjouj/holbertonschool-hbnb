87e01e0b90cf9e9dcbce8e0c231b2de1068f82ad
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-23 15:14:53 +0200

    fix: remove the function and put it in index file

--- part4/scripts.js
+++ part4/scripts.js
@@ -6,31 +6,3 @@
 document.addEventListener('DOMContentLoaded', () => {
 fetchPlaces();  
 });
-
-function fetchPlaces() {
-    fetch('http://localhost:5000/api/v1')
-        .then(response => response.json())
-        .then(data => {
-            displayPlaces(data);
-        })
-        .catch(error => {
-            console.error('Error fetching places:', error);
-        });
-}
-  function displayPlaces(places) {
-    const placesList = document.getElementById('places-list');
-    placesList.innerHTML = '';
-
-    places.forEach(place => {
-        const card = document.createElement('div');
-        card.className = 'place-card';
-
-        card.innerHTML = `
-            <h3>${place.name}</h3>
-            <p>Price per night: $${place.price}</p>
-            <button class="details-button" data-id="${place.id}">View Details</button>
-        `;
-
-        placesList.appendChild(card);
-    });
-}
