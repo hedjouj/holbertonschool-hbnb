880487ba0e5388aecdc27febefcd8276777820d2
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-23 15:14:06 +0200

    fix: added the route places to GET the places list for the index

--- part3/run.py
+++ part3/run.py
@@ -4,5 +4,14 @@ from flask_cors import CORS
 app = create_app('development')
 CORS(app)
 
+@app.route('/api/v1/places', methods=['GET'])
+def get_places():
+    places = [
+        {"id": 1, "name": "Place 1", "price": 100},
+        {"id": 2, "name": "Place 2", "price": 150}
+    ]
+    return {"places": places}
+
+
 if __name__ == '__main__':
     app.run(debug=True)
