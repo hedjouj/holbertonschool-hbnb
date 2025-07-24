bc3744f7d1032f539cc49604459cf8fe5b1d0841
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-24 13:58:05 +0200

    feat: remove the route that cause bug

--- part3/run.py
+++ part3/run.py
@@ -1,17 +1,14 @@
 from app import create_app
 from flask_cors import CORS
+from app.extensions import db
+
 
 app = create_app('development')
 CORS(app)
-
-@app.route('/api/v1/places', methods=['GET'])
-def get_places():
-    places = [
-        {"id": 1, "name": "Place 1", "price": 100},
-        {"id": 2, "name": "Place 2", "price": 150}
-    ]
-    return {"places": places}
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
