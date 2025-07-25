a31fa3ecca1c05b086daafff1a7673c45ad7fba3
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-25 10:54:44 +0200

    fix: remove an useless line and make sure the app run on port 5000

--- part3/run.py
+++ part3/run.py
@@ -4,11 +4,10 @@ from app.extensions import db
 
 
 app = create_app('development')
-CORS(app)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
