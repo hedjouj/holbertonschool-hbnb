773608857f2f1a65e62f928bb0e4a102de1de586
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-27 14:10:36 +0200

    fix: added the foreign key to associate the table amenity with places

--- part3/app/models/association_tables.py
+++ part3/app/models/association_tables.py
@@ -2,8 +2,6 @@ from app import db
 
 place_amenity = db.Table(
     'place_amenity',
-    db.Column('place_id', db.String(36),
-              db.ForeignKey('places.id'), primary_key=True),
-    db.Column('amenity_id', db.String(36),
-              db.ForeignKey('amenities.id'), primary_key=True)
+    db.Column('place_id', db.String(60), db.ForeignKey('places.id'), primary_key=True, nullable=False),
+    db.Column('amenity_id', db.String(60), db.ForeignKey('amenities.id'), primary_key=True, nullable=False)
 )
